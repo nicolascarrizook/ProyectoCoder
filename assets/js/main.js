b57ccda8471adc6936c39ffd189b7172100ee5cf
nicolascarrizo <nicolascarrizook@gmail.com>
2021-05-31 17:49:18 -0300

    Se suben todos los archivos al repositorio

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+/*Tooltip*/
+
+$(function() {
+    $('.recurso__image').tooltip();
+})
