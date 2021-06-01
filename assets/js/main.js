18cbece50d40c7d2e351388f9d0ba35b8a44e2c2
nicolascarrizo <nicolascarrizook@gmail.com>
2021-06-01 20:11:43 -0300

    Se sube estilos para el menu responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,14 @@ $('.owl-carousel').owlCarousel({
 $(function() {
     $('.recurso__image').tooltip();
 })
+
+/*Menu*/
+
+const menuBtn = document.querySelector('.boton__nav');
+const menuList = document.querySelector('.navigation__nav');
+/* Menu toggle */
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("open");
+    menuList.classList.toggle("open");
+});
