a57cc58e3cd43dc865839adb3a3c930a7b7d932d
nicolascarrizo <nicolascarrizook@gmail.com>
2021-06-06 21:02:22 -0300

    Se actaliza web

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ $('.owl-carousel').owlCarousel({
             items:1
         },
         600:{
-            items:2
+            items:1
         },
         1000:{
             items:3
@@ -21,3 +21,14 @@ $('.owl-carousel').owlCarousel({
 $(function() {
     $('.recurso__image').tooltip();
 })
+
+/*Menu*/
+
+const menuBtn = document.querySelector('.boton__nav');
+const menuList = document.querySelector('.navigation__nav');
+/* Menu toggle */
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("open");
+    menuList.classList.toggle("open");
+});
